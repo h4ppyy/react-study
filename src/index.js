3148737c64a4e9e7cad4cbc326137bd741cffe86
h4ppyy <93immm@naver.com>
2020-04-09 15:05:28 +0900

    react persist

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { createStore } from 'redux'
-import rootReducer from './components/reducers'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
-)
+import configureStore from './components/store';
+
+const { store, persistor } = configureStore();
+// persistor.purge();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>, 
   document.getElementById('root')
 );
